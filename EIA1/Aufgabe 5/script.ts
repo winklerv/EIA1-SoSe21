684b30b55b1ccfd6e72cf46cb44752daf99dd969
winklerv <vivian.erja.selin.winkler@hs-furtwangen.de>
2021-05-02 15:21:11 +0200

    a5

--- EIA1/Aufgabe 5/script.ts
+++ EIA1/Aufgabe 5/script.ts
@@ -1,24 +1,24 @@
-var africa = "Afrika";
-var australia = "Australien";
-var southamerica = "Südamerika";
-var northamerica = "Nordamerika";
-var asia = "Asien";
-var europe = "Europa";
+var africa : string  = "Afrika";
+var australia : string = "Australien";
+var southamerica : string = "Südamerika";
+var northamerica : string = "Nordamerika";
+var asia : string = "Asien";
+var europe : string = "Europa";
 
-var africa2008 = 1028;
-var africa2018 = 1235.5;
-var australia2008 = 1993;
-var australia2018 = 2100.5;
-var southamerica2008 = 1132.6;
-var southamerica2018 = 1261.5;
-var northamerica2008 = 6600.4;
-var northamerica2018 = 6035.6;
-var asia2008 = 12954.7;
-var asia2018 = 16274.2;
-var europe2008 = 4965.7;
-var europe2018 = 4209.3;
+var africa2008 : number = 1028;
+var africa2018 : number = 1235.5;
+var australia2008 : number = 1993;
+var australia2018 : number = 2100.5;
+var southamerica2008 : number = 1132.6;
+var southamerica2018 : number = 1261.5;
+var northamerica2008 : number = 6600.4;
+var northamerica2018 : number = 6035.6;
+var asia2008 : number = 12954.7;
+var asia2018 : number = 16274.2;
+var europe2008 : number = 4965.7;
+var europe2018 : number  = 4209.3;
 
-var entire2018 = (africa2018 + australia2018 + southamerica2018 + northamerica2018+ asia2018 + europe2018) 
+var entire2018 : number = (africa2018 + australia2018 + southamerica2018 + northamerica2018+ asia2018 + europe2018) 
 
 console.log("Die Emission von " + africa + " ist: " + africa2018 + "kg CO2");
 console.log("Relativ zu Gesamtemission der Welt verursacht " + africa + " damit " + Math.round(africa2018 / entire2018 * 100 * 100) / 100 + "%");
